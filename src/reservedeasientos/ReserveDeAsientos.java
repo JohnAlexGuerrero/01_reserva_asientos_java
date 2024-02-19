b16908d9b7f7041afae4ac735ff6a3ca3319aa1e
johnalexguerrero <johnalexguerrero@gmail.com>
2024-02-19 10:43:25 -0500

    second commit

--- src/reservedeasientos/ReserveDeAsientos.java
+++ src/reservedeasientos/ReserveDeAsientos.java
@@ -54,11 +54,92 @@ public class ReserveDeAsientos {
                 }else{
                     continue;
                 }
-                
             }
             
         } catch (Exception e) {
             return false;
+        }
+        
+        return bandera;
+    }
+    
+    static boolean asignarAsiento(String asiento, char asientos[][]){
+        int puesto = Integer.parseInt(asiento.substring(1));
+        boolean bandera = true;
+        
+        switch (asiento.charAt(0)) {
+            case 'A':
+                if(asientos[puesto - 1][0] == 'L'){
+                    asientos[puesto - 1][0] = 'X';
+                }else{
+                    bandera = false;
+                }
+                break;
+            case 'B':
+                if(asientos[puesto - 1][1] == 'L'){
+                    asientos[puesto - 1][1] = 'X';
+                }else{
+                    bandera = false;
+                }
+                break;
+            case 'C':
+                if(asientos[puesto - 1][2] == 'L'){
+                    asientos[puesto - 1][2] = 'X';
+                }else{
+                    bandera = false;
+                }
+                break;
+            case 'D':
+                if(asientos[puesto - 1][3] == 'L'){
+                    asientos[puesto - 1][3] = 'X';
+                }else{
+                    bandera = false;
+                }
+                break;
+            case 'E':
+                if(asientos[puesto - 1][4] == 'L'){
+                    asientos[puesto - 1][4] = 'X';
+                }else{
+                    bandera = false;
+                }
+                break;
+            case 'F':
+                if(asientos[puesto - 1][5] == 'L'){
+                    asientos[puesto - 1][5] = 'X';
+                }else{
+                    bandera = false;
+                }
+                break;
+            case 'G':
+                if(asientos[puesto - 1][6] == 'L'){
+                    asientos[puesto - 1][6] = 'X';
+                }else{
+                    bandera = false;
+                }
+                break;
+            case 'H':
+                if(asientos[puesto - 1][7] == 'L'){
+                    asientos[puesto - 1][7] = 'X';
+                }else{
+                    bandera = false;
+                }
+                break;
+            case 'I':
+                if(asientos[puesto - 1][8] == 'L'){
+                    asientos[puesto - 1][8] = 'X';
+                }else{
+                    bandera = false;
+                }
+                break;
+            case 'J':
+                if(asientos[puesto - 1][9] == 'L'){
+                    asientos[puesto - 1][9] = 'X';
+                }else{
+                    bandera = false;
+                }
+                break;
+            default:
+                throw new AssertionError();    
         }
         
         return bandera;
@@ -76,17 +157,27 @@ public class ReserveDeAsientos {
             }
         }
         
-        //mostrar asientos
-        mostrarAsientos(asientos);
+        do {            
+            
+            //mostrar asientos
+            mostrarAsientos(asientos);
+
+            System.out.print("\ningresa un asiento o exit para terminar: ");
+            asiento = enter.nextLine().toUpperCase();
+
+            if(validarAsientoLetra(asiento.charAt(0)) && validarAsientoNumero(asiento)){
+                boolean bandera = asignarAsiento(asiento, asientos);
+
+                if(bandera){
+                    System.out.println("asiento asignado.");
+                }else{
+                    System.out.println("El asiento se encuentra ocupado.");
+                }
+            }else{
+                System.err.println("Error al asignar un asiento");
+            }
+        } while (!asiento.equals("EXIT"));
         
-        System.out.print("\ningresa un asiento: ");
-        asiento = enter.nextLine();
-        
-        if(validarAsientoLetra(asiento.charAt(0)) && validarAsientoNumero(asiento)){
-            System.out.println("asiento asignado.");
-        }else{
-            System.err.println("Error al asignar un asiento");
-        }
     }
     
 }
